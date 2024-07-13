d1317d80e1ce9a33092a978e3ce0b6267c4b9ceb
Andy <andyalt20@gmail.com>
2024-07-13 23:30:05 +0400

    Remove MVC Components

--- BasicMVCApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BasicMinimalApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
